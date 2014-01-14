71953155b4cceb69dfe3c62bda633de0356dfaac
bruno <bnsantos17@gmail.com>
2014-01-14 20:06:18 -0200

    Edited img input file, added colors constant file. Maze file adding start, end and finding those points

--- maze/maze.py
+++ maze/maze.py
@@ -1,17 +1,44 @@
 __author__ = 'bruno'
 from PIL import Image
+import colors
 
 
 class Maze(object):
-    def __init__(self, filename):
-        self.base_img = Image.open(filename)
+    def __init__(self, input_filename, output_filename):
+        self.input_filename = input_filename
+        self.output_filename = output_filename
+        self.base_img = Image.open(self.input_filename)
         self.base_pixels = self.base_img.load()
         self.start_pos = None
         self.end_pos = None
         self.solved = False
+        self.find_start_and_end_point()
 
     def is_solved(self):
         return self.solved
 
+    def set_end_point(self, x, y):
+        self.set_point_color(x, y, colors.GREEN)
+
+    def set_start_point(self, x, y):
+        self.set_point_color(x, y, colors.BLUE)
+
+    def set_point_color(self, x, y, color):
+        self.base_pixels[x, y] = color
+        #overriding picture
+        self.base_img.save(self.input_filename)
+
     def find_start_and_end_point(self):
-        pass
+        width = self.base_img.size[0]
+        height = self.base_img.size[1]
+
+        for i in range(width):
+            for j in range(height):
+                pixel = self.base_pixels[i, j]
+                if pixel != colors.WHITE and pixel != colors.BLACK:
+                    if pixel == colors.BLUE:
+                        self.start_pos = (i, j)
+                    elif pixel == colors.GREEN:
+                        self.end_pos = (i, j)
+        if self.start_pos is None or self.end_pos is None:
+            print 'ERROR'
--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 __author__ = 'bruno'
 from maze.maze import Maze
 
-m = Maze('input/simple.png')
+m = Maze('input/simple.png', 'output/solution.png')
+#m.set_end_point(10, 20)
+#m.set_start_point(540, 400)
 
