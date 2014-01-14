296330ec52e40959d671e711ab4786f93470aab8
bruno <bnsantos17@gmail.com>
2014-01-14 21:19:45 -0200

    small, medium and big input example

--- run.py
+++ run.py
@@ -1,9 +1,23 @@
 __author__ = 'bruno'
 from maze.maze import Maze
 
-m = Maze('input/simple.png', 'output/solution.png')
-m.solve()
-m.save_solved()
+
+m = Maze('input/simple.png', 'output/solution_simple.png')
+#small set up
 #m.set_end_point(10, 20)
 #m.set_start_point(540, 400)
+
+#m = Maze('input/medium_maze.png', 'output/solution_medium.png')
+#medium set up
+#m.set_end_point(175, 175)
+#m.set_start_point(5, 5)
+
+#m = Maze('input/bigger_maze.png', 'output/solution_bigger.png')
+#bigger set up
+#m.set_end_point(1990, 605)
+#m.set_start_point(5, 530)
+
+m.solve()
+m.save_solved()
+
 
