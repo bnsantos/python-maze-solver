b7b28b1dbf87f419cbaf50150b55ec826a6d337b
bruno <bnsantos17@gmail.com>
2014-01-14 20:54:07 -0200

    Now is printing solution in output picture. Adding output picture

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from maze.maze import Maze
 
 m = Maze('input/simple.png', 'output/solution.png')
 m.solve()
-#m.saveSolved()
+m.save_solved()
 #m.set_end_point(10, 20)
 #m.set_start_point(540, 400)
 
